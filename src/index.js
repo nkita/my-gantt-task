c9f4a3d810b9fbb9491429ca1d71729310665312
nkita <nkitam7@gmail.com>
2022-03-20 22:38:38 +0900

    ローカルリポジトリのreactプロジェクトを読み込む

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Gantt } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+
+const tasks = [
+  {
+    start: new Date(2020, 1, 1),
+    end: new Date(2020, 1, 2),
+    name: "Idea",
+    id: "Task 0",
+    type: "task",
+    progress: 45,
+    isDisabled: true,
+    styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
+  },
+];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Gantt
+    viewMode={"Day"}
+    viewDate={20220317}
+    tasks={tasks} />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
